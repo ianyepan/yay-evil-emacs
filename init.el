3e9342fb158df39bf1179e79e79771efdc23a108
ianpan870102 <ianpan870102@gmail.com>
2018-10-22 22:10:18 +0800

    Initial commit

---
+++ init.el
@@ -0,0 +1,26 @@
+;;; .emacs --- Custom Emacs Configuration
+;;  Author: Ian Y.E. Pan
+;;; Commentary:
+;;; Code:
+(require 'package)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'load-path "~/.emacs.d/evil")
+(setq package-enable-at-startup nil)
+(require 'cl)
+
+;; Load theme by hand, don't use 'M-x customize-themes'
+;;(add-to-list `custom-theme-load-path "~/.emacs.d/themes/")
+;;(load-theme `tronlegacy t)
+(load-theme 'base16-solarized-dark t)
+
+;; Load actual config file
+(require 'org)
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+;; Load custom set faces and variables
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+(provide 'init)
+;; init.el ends here
