5cb8767cd250fa0cb87bcda6938228538b397b8e
ianpan870102 <ianpan870102@gmail.com>
2018-10-27 12:37:04 +0800

    Added Drag-Stuff.

--- config.el
+++ config.el
@@ -307,28 +307,8 @@
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 (define-key mc/keymap (kbd "<return>") nil)
 
-(defun move-region (start end n)
-  "Move the current region up or down by N lines."
-  (interactive "r\np")
-  (let ((line-text (delete-and-extract-region start end)))
-    (forward-line n)
-    (let ((start (point)))
-      (insert line-text)
-      (setq deactivate-mark nil)
-      (set-mark start))))
-
-(defun move-region-up (start end n)
-  "Move the current line up by N lines."
-  (interactive "r\np")
-  (move-region start end (if (null n) -1 (- n))))
-
-(defun move-region-down (start end n)
-  "Move the current line down by N lines."
-  (interactive "r\np")
-  (move-region start end (if (null n) 1 n)))
-
-(global-set-key (kbd "M-<up>") 'move-region-up)
-(global-set-key (kbd "M-<down>") 'move-region-down)
+(drag-stuff-global-mode 1)
+(drag-stuff-define-keys)
 
 ;; Go to end of line, forward-delete char, insert space.
 (fset 'join-lines
