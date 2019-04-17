25ec87e563c4d96758db3e0ed9d997037a0fd72f
ianpan870102 <ianpan870102@gmail.com>
2019-04-18 05:50:49 +0800

    Added transparency to start-up

--- config.el
+++ config.el
@@ -275,17 +275,6 @@
      nil 'alpha
      (if (eql (cond ((numberp alpha) alpha)
                     ((numberp (cdr alpha)) (cdr alpha))
-                    ((numberp (cadr alpha)) (cadr alpha))) 100)
-         '(75 . 75) '(100 . 100)))))
-(global-set-key (kbd "C-c t") 'toggle-transparency)
-
-(defun toggle-transparency ()
-  (interactive)
-  (let ((alpha (frame-parameter nil 'alpha)))
-    (set-frame-parameter
-     nil 'alpha
-     (if (eql (cond ((numberp alpha) alpha)
-                    ((numberp (cdr alpha)) (cdr alpha))
                     ;; Also handle undocumented (<active> <inactive>) form.
                     ((numberp (cadr alpha)) (cadr alpha)))
               100)
