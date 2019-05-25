2dd48e90e0315bf67df9e577527b8bfe7dd6ab96
ianpan870102 <ianpan870102@gmail.com>
2019-05-25 11:28:18 +0800

    temporarily change font to 'Hack'

--- custom.el
+++ custom.el
@@ -60,7 +60,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Andale Mono"))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Hack"))))
  '(variable-pitch ((t (:height 1.3 :family "Garamond")))))
 
 (provide 'custom)
