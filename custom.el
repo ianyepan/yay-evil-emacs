ef02c3861db7a5316653f1ffe0f9e8b6ca3d48a8
Ian Y.E. Pan <ianyepan@Ians-MacBook-Pro.local>
2019-06-10 23:14:13 +0800

    remove unnecessary default face (font) parameters

--- custom.el
+++ custom.el
@@ -15,7 +15,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Menlo")))))
+ '(default ((t (:height 140 :family "Menlo")))))
 
 (provide 'custom)
 ;;; custom.el ends here
