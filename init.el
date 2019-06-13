5bc257c0518856c7d4b415f1e77b3455f2bc940f
Ian Y.E. Pan <ianyepan@Ians-MacBook-Pro.local>
2019-06-13 10:53:45 +0800

    add org to selected packages

--- init.el
+++ init.el
@@ -6,15 +6,13 @@
 
 ;; Make sure selected packages are all there
 (require 'package)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(setq package-selected-packages '(which-key ranger company evil magit smooth-scrolling rainbow-mode org-bullets ido-vertical-mode highlight-operators highlight-numbers highlight-escape-sequences flycheck auto-indent-mode))
+(setq package-selected-packages '(org which-key ranger company evil magit smooth-scrolling rainbow-mode org-bullets ido-vertical-mode highlight-operators highlight-numbers highlight-escape-sequences flycheck auto-indent-mode))
+(setq package-enable-at-startup nil)
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 (package-install-selected-packages)
-(setq package-enable-at-startup nil)
-(package-initialize)
 
 ;; Load theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
