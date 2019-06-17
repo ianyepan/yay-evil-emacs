1b1412b8a22e5eed5f641e1959d06d85eb53c218
Ian Y.E. Pan <ianyepan@Ians-MacBook-Pro.local>
2019-06-17 16:21:54 +0800

    maximize frame when start-up

--- custom.el
+++ custom.el
@@ -7,6 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(package-selected-packages
    (quote
     (yasnippet yasnippet-snippets org which-key ranger company evil magit smooth-scrolling rainbow-mode org-bullets ido-vertical-mode highlight-operators highlight-numbers highlight-escape-sequences flycheck auto-indent-mode))))
