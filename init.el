98e5b478eedbacb822c34d39aef675c1eeaaf337
Ian Y.E. Pan <ianyepan@Ians-MacBook-Pro.local>
2019-06-18 11:13:05 +0800

    avoid garbage collection and check for proper handler during startup

--- init.el
+++ init.el
@@ -4,11 +4,16 @@
 ;;; A personalized configuration file tailored to suit my needs
 ;;; Code:
 
-(require 'package) ;; load package manager
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+(defvar ian--file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
 (setq package-selected-packages '(dashboard yasnippet yasnippet-snippets which-key ranger company evil magit smooth-scrolling rainbow-mode org-bullets ido-vertical-mode highlight-operators highlight-numbers highlight-escape-sequences flycheck auto-indent-mode))
-(package-initialize) ;; load and activate packages
+(package-initialize)
 (setq package-enable-at-startup nil)
 (unless package-archive-contents
   (package-refresh-contents))
@@ -24,6 +29,10 @@
 
 ;; Dump custom-set-variables to a garbage file and don't load it
 (setq custom-file "~/.emacs.d/package-selected-packages.el")
+
+(setq gc-cons-threshold 16777216
+      gc-cons-percentage 0.1)
+(setq file-name-handler-alist ian--file-name-handler-alist)
 
 (provide 'init)
 ;;; init.el ends here
